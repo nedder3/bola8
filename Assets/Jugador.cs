1c4c903d9ea9b6a1e938bc40628e3f7a6503b686
nedder3 <arij.dev@gmail.com>
2023-04-18 17:28:23 -0300

    ADD obstacles and walls

--- Assets/Jugador.cs
+++ Assets/Jugador.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Jugador : MonoBehaviour
 {
@@ -10,19 +11,32 @@ public class Jugador : MonoBehaviour
         
     }
 
+    private void OnCollisionEnter(Collision collision)
+     {
+        //reset scene
+        SceneManager.LoadScene(0);
+     }
+
+
     // Update is called once per frame
     void Update()
     {
         //movement to left
         if(Input.GetKey(KeyCode.A))
         {
-            GetComponent<Rigidbody>().AddForce(Vector3.left*500*Time.deltaTime);
+            GetComponent<Rigidbody>().AddForce(Vector3.left * 500 * Time.deltaTime);
         }
 
          //movement to right
         if(Input.GetKey(KeyCode.D))
         {
-            GetComponent<Rigidbody>().AddForce(Vector3.right*500*Time.deltaTime);
+            GetComponent<Rigidbody>().AddForce(Vector3.right * 500 * Time.deltaTime);
+        }
+
+           //movement to foward
+        if(Input.GetKey(KeyCode.W))
+        {
+           transform.Translate(Vector3.forward * 5 *Time.deltaTime);
         }
 
         
